566f525e0a7dbc3fe92112918776c06ca161016e
Marcus Myrick <mjmyrick12@gmail.com>
2023-01-01 14:10:40 -0600

    import complete

--- server/index.js
+++ server/index.js
@@ -6,6 +6,11 @@ import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
+import clientRoutes from "./routes/client.js"
+import generalRoutes from "./routes/general.js"
+import managementRoutes from "./routes/management.js"
+import salesRoutes from "./routes/sales.js"
+
 
 // configuation 
 
