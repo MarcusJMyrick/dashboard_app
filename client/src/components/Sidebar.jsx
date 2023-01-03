0eaea99a92acf6f28738107b96d90bceb23c6eac
Marcus Myrick <mjmyrick12@gmail.com>
2023-01-03 00:32:54 -0600

    need to fix user names and occupation error

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -196,7 +196,15 @@ const Sidebar = ({
           <Box position="absolute" bottom="2rem">
             <Divider />
             <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
-              
+              <Box
+                component="img"
+                alt="profile"
+                src={profileImage}
+                height="40px"
+                width="40px"
+                borderRadius="50%"
+                sx={{ objectFit: "cover" }}
+              />
               <Box textAlign="left">
                 <Typography
                   fontWeight="bold"
@@ -212,7 +220,12 @@ const Sidebar = ({
                   {user.occupation}
                 </Typography>
               </Box>
-              
+              <SettingsOutlined
+                sx={{
+                  color: theme.palette.secondary[300],
+                  fontSize: "25px ",
+                }}
+              />
             </FlexBetween>
           </Box>
         </Drawer>
